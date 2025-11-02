417909a07c974ca53f4c5e7331fa875d60a7fd2a
shahkrishh18 <krishpvt01@gmail.com>
2025-11-03 01:26:11 +0530

    netlify added

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,7 @@ const app = express();
 // Middleware
 const allowedOrigins = [
   'https://movie-theatre-one.vercel.app',
+  'https://moviemaxtime.netlify.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173'
 ];
